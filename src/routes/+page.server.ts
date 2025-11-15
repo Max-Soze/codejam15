45d96c47a15fa4aae170cb8c20e272098a17052d
Max <46341774+Max-Soze@users.noreply.github.com>
2025-11-15 14:37:11 -0500

    added popup functionality

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,11 +2,16 @@ import type { PageServerLoad } from './$types'
 import { mongo } from '$lib/server/mongo'
 
 export const load: PageServerLoad = async () => {
+    // Load tasks from Mongo
     const client = await mongo
     const database = client.db("TaskTown")
     const tasks = database.collection("tasks")
     let count = await tasks.countDocuments()
     let allTasks = await tasks.find({}).toArray()
     let taskList = JSON.parse(JSON.stringify(allTasks))
-    return {count, taskList}
+
+    // Load User Data from Mongo
+    let userData = await database.collection("users").findOne({})
+    let user = JSON.parse(JSON.stringify(userData))
+    return {count, taskList, user}
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,27 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
+	import Popup from '$lib/components/Popup.svelte';
+
+	let open = $state(false);
 
 	let { data }: PageProps = $props();
 </script>
 
-<p>There are currently {data.count} tasks registered.</p>
-{#each data.taskList as task}
-	<br/>
-	{#each Object.entries(task) as [ key, value ]}
-		<p>{key}: {value}</p>
-	{/each}
+{#each Object.entries(data.user) as [key, value]}
+	<p>{key}: {value}</p>
 {/each}
+<br/>
+<p>There are currently {data.count} tasks registered.</p>
+<button onclick={() => open = true}>Open</button>
+<br/>
+{#if open}
+	<Popup>
+		{#each data.taskList as task}
+			<br/>
+			{#each Object.entries(task) as [ key, value ]}
+				<p>{key}: {value}</p>
+			{/each}
+		{/each}
+		<button onclick={() => open = false}>Close</button>
+	</Popup>
+{/if}
