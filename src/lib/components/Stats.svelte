9d4db9197ddb34ef18a29e0b95a4fa05189d8ee5
Max <46341774+Max-Soze@users.noreply.github.com>
2025-11-16 03:19:29 -0500

    add top panel data

---
+++ src/lib/components/Stats.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { DiscreteInterpolant } from 'three';
+
+	let { social, health, discipline, intellect } = $props();
+</script>
+
+<div>
+	<p>{social}</p>
+	<p>{health}</p>
+	<p>{discipline}</p>
+	<p>{intellect}</p>
+</div>
