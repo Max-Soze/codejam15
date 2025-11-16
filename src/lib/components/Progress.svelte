04b29630d0c2e193f89cfb30aa6a1ffebc0d0d5f
ldseou <elodiesu2005@gmail.com>
2025-11-16 04:01:08 -0500

    adding progress bars

---
+++ src/lib/components/Progress.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    
+    export let value: number = 0;
+    export let color :string = "#000000";
+</script>
+
+<div class = "progress">
+    <div class="bar" style="width:{value}%; background:{color};"></div>
+</div>
+
+<style>
+    .progress{
+        width:100%;
+        height:10px;
+        background-color: beige;
+        border-radius: 2px;
+        overflow: hidden;
+    }
+    .bar{
+        height:100%;
+        transition: width 0.25s ease;
+    }
+</style>
